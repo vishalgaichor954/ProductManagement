1d61cf97929e05d97a11697f81b83eee77840432
vishal vishram gaichor <vishalgaichor954@outlook.com>
2021-04-21 01:39:15 +0530

    New Controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using ProductManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductManagement.Controllers
+{
+    public class UserController : ApiController
+    {
+        ProductManagementEntities managementEntities = new ProductManagementEntities();
+        // GET api/<controller>
+        public IEnumerable<string> Index()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
